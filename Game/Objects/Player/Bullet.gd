052adf5063ea8db3735a17a284cccb02f25264c0
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-23 20:57:23 +0200

    Fixup bullets

--- Game/Objects/Player/Bullet.gd
+++ Game/Objects/Player/Bullet.gd
@@ -8,10 +8,10 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 
 func _on_body_entered(body: Node2D) -> void:
-	body.get_script()
-	body.queue_free()
+	if body.name == "Enemy":
+		body.queue_free()
 	self.queue_free()
 
 
 func _on_screen_exited() -> void:
-	self.queue_free() # Replace with function body.
+	self.queue_free()
