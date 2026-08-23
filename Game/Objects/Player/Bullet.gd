7826d8ea12d41452b432111726a03b2e713ffb3f
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-23 18:42:28 +0200

    First player gameplay

---
+++ Game/Objects/Player/Bullet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var Speed : float = 500.0
+
+func _process(delta: float) -> void:
+	var direction = Vector2.RIGHT.rotated(rotation)
+	var velocity = direction * Speed
+	position += velocity * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	body.queue_free()
+	self.queue_free()
--- Game/Objects/Player/Player.gd
+++ Game/Objects/Player/Player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 @export var Attack: PackedScene
+@export var MoveSpeed: float = 30000.0
+@export var AttackSpeed: float = 1.0
 
-const MoveSpeed = 30000.0
 var EnableAttack = false
+var AttackTimer = 0.0
 
 func _physics_process(delta: float) -> void:
 	var vertical = delta * MoveSpeed * Input.get_axis("Player_Move_Up","Player_Move_Down")
@@ -25,9 +27,14 @@ func _input(event):
 			EnableAttack = false
 
 func _process(delta: float) -> void:
-	if EnableAttack == false:
-		return
+	if AttackTimer > 0.0:
+		AttackTimer -= delta
+
+	if EnableAttack && AttackTimer <= 0.0:
+		_attack()
 	
+func _attack():
 	var attack = Attack.instantiate()
 	owner.add_child(attack)
-	attack.position = $Marker2D.global_position
+	attack.transform = $Marker2D.global_transform
+	AttackTimer += 1.0 / AttackSpeed
