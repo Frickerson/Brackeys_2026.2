e38d9ee72f63f3f4320d4a68435fd322c21a0cfb
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-24 20:42:36 +0200

    Add move ranges

--- Game/Objects/Enemy/Enemy.gd
+++ Game/Objects/Enemy/Enemy.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 class_name Enemy
 
+@export var NavigationAgent : NavigationAgent2D
 @export var DefaultWeapon: PackedScene
-@export var AttackRange: float = 500.0
+@export var AttackRange: float = 200.0
 @export var AttackLocation: Node2D
 @export var RayCast : RayCast2D
 @export var MoveSpeed : float = 200.0
+@export var MinMoveRange : float = 100
+@export var MaxMoveRange : float = 300
 
-@onready var NavigationAgent : NavigationAgent2D = $NavigationAgent2D 
 @onready var PlayerRef : Player = get_tree().get_first_node_in_group("Player")
 
 var EquippedWeapon: Weapon = null
@@ -19,12 +21,16 @@ func _ready() -> void:
 		AttackLocation.add_child(EquippedWeapon)
 		
 	RayCast.add_exception(PlayerRef)
+	NavigationAgent.target_desired_distance = MinMoveRange
 
 func _physics_process(delta: float) -> void:
 	if PlayerRef == null:
 		return
 
 	var player_position = PlayerRef.global_position
+	if !_in_move_range(player_position):
+		return
+		
 	_move_towards_player(player_position)
 	
 	var enable_attack = _in_attack_range(player_position) && _in_line_of_sight(player_position)
@@ -35,7 +41,7 @@ func _take_damage(damage : float) -> void:
 	
 func _move_towards_player(player_position : Vector2) -> void:
 	NavigationAgent.target_position = player_position
-	
+		
 	if NavigationAgent.is_navigation_finished():
 		return
 	
@@ -57,3 +63,7 @@ func _in_attack_range(player_position : Vector2) -> bool:
 func _in_line_of_sight(player_position : Vector2) -> bool:
 	RayCast.target_position = RayCast.to_local(player_position)
 	return !RayCast.is_colliding()
+	
+func _in_move_range(player_position : Vector2) -> bool:
+	var distance = global_position.distance_squared_to(player_position)
+	return distance <= MaxMoveRange * MaxMoveRange
