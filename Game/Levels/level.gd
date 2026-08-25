e8afb7ce79c7e7fbcb6749b32c3e83dd42d663a1
Jiji <jihad.bou15@outlook.com>
2026-08-25 13:56:14 +0200

    level changes

--- Game/Levels/level.gd
+++ Game/Levels/level.gd
@@ -1,1 +1,18 @@
 extends NavigationRegion2D
+class_name Level
+
+signal _on_win;
+@export var alternativeTimerWin = 30
+
+var enemyAmount: int
+
+func _ready() -> void:
+	enemyAmount = get_tree().get_node_count_in_group("Enemy")
+	if alternativeTimerWin > 0:
+		$AlternativeWin.start(alternativeTimerWin)
+
+
+
+func _on_alternative_win_timeout() -> void:
+	_on_win.emit();
+	pass # Replace with function body.
