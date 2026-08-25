bcab66027b22417c1e765a4c9b8774d335d70c92
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-25 12:38:10 +0200

    Start rework

---
+++ Game/Objects/Characters/Player/Player.gd
@@ -0,0 +1,31 @@
+extends CharacterBase
+
+class_name Player
+
+@export var Camera: Camera2D
+
+func _physics_process(delta: float) -> void:
+	var vertical = Input.get_axis("Player_Move_Up","Player_Move_Down")
+	var horizontal = Input.get_axis("Player_Move_Left","Player_Move_Right")
+	
+	velocity = Vector2(horizontal, vertical).normalized() * MoveSpeed
+	IsMoving = !velocity.is_zero_approx()
+
+	move_and_slide()
+	
+	var mouse_position = Camera.get_global_mouse_position()
+	var direction = mouse_position - position
+	rotation = direction.angle()
+	
+func _input(event):
+	if EquippedWeapon == null:
+		return
+		
+	if event.is_action("Player_Shoot"):
+		if event.pressed:
+			EquippedWeapon._toggle_attack(true)
+		if event.is_released():
+			EquippedWeapon._toggle_attack(false)
+	
+	if event.is_action("Player_Reload"):
+		EquippedWeapon._reload()
