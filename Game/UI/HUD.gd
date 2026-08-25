7efa1a08520b388f3acffbec24a95726c8bb09a5
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-25 16:35:46 +0200

    Fix crash when player dies

--- Game/UI/HUD.gd
+++ Game/UI/HUD.gd
@@ -5,8 +5,14 @@ extends CanvasLayer
 @onready var PlayerRef : Player = get_tree().get_first_node_in_group("Player")
 
 func _ready() -> void:
+	if PlayerRef == null:
+		return
+		
 	DistrustMeterRef._set_distrust_value( PlayerRef.Distrust )
 
 func _process(delta: float) -> void:
+	if PlayerRef == null:
+		return
+		
 	DistrustMeterRef._set_distrust_value( PlayerRef.Distrust )
 	
