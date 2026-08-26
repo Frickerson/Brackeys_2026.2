ecdbf0b1626f928006dfdc45ee876fced84eb397
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-26 12:32:05 +0200

    Bullet color tweaks

--- Game/Objects/Bullets/Bullet.gd
+++ Game/Objects/Bullets/Bullet.gd
@@ -6,6 +6,7 @@ class_name Bullet
 @export var FakeColor : Color = Color.RED
 @export var FakeColorCurve : float = 0.4
 @export var FadeOutCurve : float = 5.0
+@export var ColorPerTeam : Dictionary[StringName, Color]
 
 var Speed : float = 0.0
 var Damage : float = 0.0
@@ -15,18 +16,21 @@ var IsFake : bool = false
 var DefaultColor : Color
 
 func _ready() -> void:
-	DefaultColor = BulletSprite.self_modulate
+	var groups = get_groups()
+	var color = ColorPerTeam.get(get_groups()[0])
+	DefaultColor = color
+	BulletSprite.modulate = color
 
 func _process(delta: float) -> void:
 	LifeTimer += delta
 	if LifeTimer >= LifeSpan:
 		queue_free()
-		
-	var desired_color = Color.TRANSPARENT
+	
 	if IsFake:
-		BulletSprite.self_modulate = lerp(DefaultColor, FakeColor, ease(LifeTimer / LifeSpan, FakeColorCurve))
-
-	BulletSprite.modulate = lerp(Color.WHITE, Color.TRANSPARENT, ease( LifeTimer / LifeSpan, FadeOutCurve))
+		BulletSprite.modulate = lerp(DefaultColor, FakeColor, ease(LifeTimer / LifeSpan, FakeColorCurve))
+	
+	var transparency = lerp(1.0, 0.0, ease( LifeTimer / LifeSpan, FadeOutCurve))
+	BulletSprite.modulate.a = transparency
 		
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation)
