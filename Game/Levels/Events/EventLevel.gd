7a9f8f285722ad7a9b5f85834255716b656b5aa2
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-26 20:57:29 +0200

    Fix issue

--- Game/Levels/Events/EventLevel.gd
+++ Game/Levels/Events/EventLevel.gd
@@ -16,7 +16,7 @@ func _set_options(options : Array[Dictionary]) -> void:
 	for option in options:
 		var new_option = _create_option()
 		new_option.text = option.Title
-		new_option.pressed.connect(_on_option_pressed.bind(new_option))
+		new_option.pressed.connect(_on_option_pressed.bind(option))
 	
 	Option.queue_free()
 
