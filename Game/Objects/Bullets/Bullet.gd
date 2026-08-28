c3469283a6210f1bec747858ef3b4ff487ac1cd9
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-28 15:08:54 +0200

    Implement homing

--- Game/Objects/Bullets/Bullet.gd
+++ Game/Objects/Bullets/Bullet.gd
@@ -7,6 +7,7 @@ class_name Bullet
 @export var FakeColorCurve : float = 0.4
 @export var FadeOutCurve : float = 5.0
 @export var ColorPerTeam : Dictionary[StringName, Color]
+@export var HomingSpeed : float = 10.0
 
 var Speed : float = 0.0
 var Damage : float = 0.0
@@ -15,6 +16,7 @@ var LifeTimer : float = 0.0
 var IsFake : bool = false
 var DefaultColor : Color
 var Deleting : bool = false
+var HomingPercentage : float = 0.0
 
 func _ready() -> void:
 	var color = ColorPerTeam.get(get_groups()[0])
@@ -37,6 +39,12 @@ func _process(delta: float) -> void:
 	BulletSprite.self_modulate.a = transparency
 		
 func _physics_process(delta: float) -> void:
+	var homing_direction = _get_homing_direction()
+	var current_direction = Vector2.RIGHT.rotated(rotation)
+	
+	var homing_test = current_direction.lerp(homing_direction, HomingPercentage * delta * HomingSpeed)
+	global_rotation = homing_test.angle()
+	
 	var direction = Vector2.RIGHT.rotated(rotation)
 	var velocity = direction * Speed
 	global_position += velocity * delta
@@ -63,8 +71,20 @@ func _on_body_entered(body: Node2D) -> void:
 	$CollisionShape2D.queue_free()
 	Deleting = true
 	
-func _initialize(damage : float, shot_speed: float, shot_life_span : float, bullet_sprite: Texture2D) -> void:
+func _initialize(damage : float, shot_speed: float, shot_life_span : float, bullet_sprite: Texture2D, homing_percentage : float) -> void:
 	Damage = damage
 	Speed = shot_speed
 	LifeSpan = shot_life_span
 	$CollisionShape2D/Sprite2D.texture = bullet_sprite
+	HomingPercentage = homing_percentage
+	
+func _get_homing_direction() -> Vector2:
+	var player_ref : Player = get_tree().get_first_node_in_group("Player")
+	
+	if is_in_group("PlayerTeam"):
+		return global_position.direction_to(player_ref.Camera.get_global_mouse_position())
+		
+	if is_in_group("EnemyTeam"):
+		return global_position.direction_to(player_ref.global_position)
+		
+	return Vector2.ZERO
