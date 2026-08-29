71209d12060f33ca8378d6aaa60651e05a2eddb9
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-29 17:19:50 +0200

    Multiplier visualizer

--- Game/Objects/Characters/Base/CharacterMultipliers.gd
+++ Game/Objects/Characters/Base/CharacterMultipliers.gd
@@ -33,23 +33,23 @@ func _add(other : CharacterMultipliers) -> void:
 	
 func _get_additive_text() -> String:
 	var result = ""
-	result += _get_modifier_text(AttackSpeedMultiplier, "Attack Speed")
-	result += _get_modifier_text(AttackRangeMultiplier, "Attack Range")
-	result += _get_modifier_text(DamageMultiplier, "Damage")
-	result += _get_modifier_text(MovementSpeedMultiplier, "Movement Speed")
-	result += _get_modifier_text(ReloadSpeedMultiplier, "Reload Speed")
-	result += _get_modifier_text(BulletSizeMultiplier, "Bullet Size")
-	result += _get_modifier_text(BulletSpeedMultiplier, "Bullet Speed")
-	result += _get_modifier_text(MaxHealthMultiplier, "Max Health")
-	result += _get_modifier_text(GoldDropMultiplier, "Gold Drop Size")
-	result += _get_modifier_text(HealthDropMultiplier, "Health Drop Size")
-	result += _get_modifier_text(HomingMultiplier, "Homing Strength")
-	result += _get_modifier_text(AccelerationMultiplier, "Acceleration")
-	result += _get_modifier_text(DecelerationMultiplier, "Deceleration")
+	result += _get_modifier_additive_text(AttackSpeedMultiplier, "Attack Speed")
+	result += _get_modifier_additive_text(AttackRangeMultiplier, "Attack Range")
+	result += _get_modifier_additive_text(DamageMultiplier, "Damage")
+	result += _get_modifier_additive_text(MovementSpeedMultiplier, "Movement Speed")
+	result += _get_modifier_additive_text(ReloadSpeedMultiplier, "Reload Speed")
+	result += _get_modifier_additive_text(BulletSizeMultiplier, "Bullet Size")
+	result += _get_modifier_additive_text(BulletSpeedMultiplier, "Bullet Speed")
+	result += _get_modifier_additive_text(MaxHealthMultiplier, "Max Health")
+	result += _get_modifier_additive_text(GoldDropMultiplier, "Gold Drop Size")
+	result += _get_modifier_additive_text(HealthDropMultiplier, "Health Drop Size")
+	result += _get_modifier_additive_text(HomingMultiplier, "Homing Strength")
+	result += _get_modifier_additive_text(AccelerationMultiplier, "Acceleration")
+	result += _get_modifier_additive_text(DecelerationMultiplier, "Deceleration")
 		
 	return result
 	
-func _get_modifier_text(multiplier : float, name : String) -> String:
+func _get_modifier_additive_text(multiplier : float, name : String) -> String:
 	if multiplier == 0.0:
 		return ""
 		
@@ -60,3 +60,51 @@ func _get_modifier_text(multiplier : float, name : String) -> String:
 		sign =  ""	
 		
 	return str(name, " ", sign, multiplier * 100, "%", "\n")
+
+func _get_total_text() -> String:
+	var result = ""
+	result += _get_modifier_total_text(AttackSpeedMultiplier, "Attack Speed")
+	result += _get_modifier_total_text(AttackRangeMultiplier, "Attack Range")
+	result += _get_modifier_total_text(DamageMultiplier, "Damage")
+	result += _get_modifier_total_text(MovementSpeedMultiplier, "Movement Speed")
+	result += _get_modifier_total_text(ReloadSpeedMultiplier, "Reload Speed")
+	result += _get_modifier_total_text(BulletSizeMultiplier, "Bullet Size")
+	result += _get_modifier_total_text(BulletSpeedMultiplier, "Bullet Speed")
+	result += _get_modifier_total_text(MaxHealthMultiplier, "Max Health")
+	result += _get_modifier_total_text(GoldDropMultiplier, "Gold Drop Size")
+	result += _get_modifier_total_text(HealthDropMultiplier, "Health Drop Size")
+	result += _get_modifier_total_text(HomingMultiplier + 1.0, "Homing Strength")
+	result += _get_modifier_total_text(AccelerationMultiplier, "Acceleration")
+	result += _get_modifier_total_text(DecelerationMultiplier, "Deceleration")
+		
+	return result
+	
+func _get_modifier_total_text(multiplier : float, name : String) -> String:
+	var total_multiplier = multiplier - 1.0
+		
+	var sign : String
+	if total_multiplier > 0.0:
+		sign =  "+"
+	else:
+		sign =  ""	
+		
+	return str(name, " ", sign, total_multiplier * 100, "%", "\n")
+	
+static func get_default() -> CharacterMultipliers:
+	var result = CharacterMultipliers.new()
+	result.AttackSpeedMultiplier = 1.0
+	result.AttackRangeMultiplier = 1.0
+	result.DamageMultiplier = 1.0
+	result.MovementSpeedMultiplier = 1.0
+	result.ReloadSpeedMultiplier = 1.0
+	result.BulletSizeMultiplier = 1.0
+	result.BulletSpeedMultiplier = 1.0
+	result.MaxHealthMultiplier = 1.0
+	result.GoldDropMultiplier = 1.0
+	result.HealthDropMultiplier = 1.0
+	result.HomingMultiplier = 0.0
+	result.AccelerationMultiplier = 1.0
+	result.DecelerationMultiplier = 1.0
+	
+	return result
+	
