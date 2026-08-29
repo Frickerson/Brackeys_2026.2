1b0534b19e911a6c33a9a5d32256808d9764a3b5
Viktor Blomme <viktor.blomme@gmail.com>
2026-08-29 15:12:41 +0200

    Fix multishot

--- Game/Objects/Weapons/Base/MultiShot.gd
+++ Game/Objects/Weapons/Base/MultiShot.gd
@@ -3,8 +3,9 @@ extends Weapon
 @export var Angle : float = 45.0
 
 func _spawn_attack(shots : int, index : int) -> Node:
-	var angle_offset = Angle / shots
-	var rotation_offset = angle_offset * index
+	var adjusted_index = index - floor(shots / 2.0)
+	var angle_offset = Angle / (shots - 1)
+	var rotation_offset = angle_offset * adjusted_index
 	var attack = _create_attack()
 	attack.global_position = global_position
 	attack.global_rotation = global_rotation + deg_to_rad(rotation_offset)
