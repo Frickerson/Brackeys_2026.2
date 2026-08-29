384f55874868915ab1fe55d853024dea91acf44d
Jiji <jihad.bou15@outlook.com>
2026-08-29 09:23:12 +0200

    boot back to main menu on win or loss

--- Game/UI/MainMenu.gd
+++ Game/UI/MainMenu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready
-var start_button = get_node("%StartButton")
-
-
-
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Game/Levels/BasicLevel.tscn")
+	get_tree().change_scene_to_file("res://Game/Map/NodeMap.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
